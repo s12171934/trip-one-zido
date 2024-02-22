e5b2c21c786a2eed31fcc5ca2dab06ced6f9f642
s12171934 <ssong99999@naver.com>
2024-02-22 17:16:17 +0900

    project base

---
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/TripOneZidoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.triponezidoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripOneZidoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripOneZidoApiApplication.class, args);
+    }
+
+}
