c06d4766c6b07904066b8ae28fa858390f78c832
MinSun <hsun0987@daum.net>
2024-02-26 17:35:25 +0900

    REFACTOR: tour.sql, ADD: tour table insert query - tour 테이블 수정으로 인한 테이블명 및 컬럼명, 쿼리 수정 - 관광정보 등록 쿼리 추가 -

--- SQL-query/tour.sql
+++ SQL-query/tour.sql
@@ -1,8 +1,8 @@
 --관광정보 전체 조회
 SELECT
   id,
-  name,
-  picture,
+  c.title,
+  p.photo,
   address,
   (
     SELECT
@@ -22,14 +22,44 @@ SELECT
       AND member_id = ?
   ) my_bookmark
 FROM
-  tour_info;
+  tour t, content c, photo p
+WHERE
+  tour.id = content.id 
+  AND tour.id = photo.content_id;
 
 --관광정보 상세 조회
 SELECT
-  *
+  t.*, 
+  c.title,
+  p.photo,
 FROM
-  tour_info
+  tour t, content c, photo p
 WHERE
-  id = ?;
+  id = ?
+  AND tour.id = content.id 
+  AND tour.id = photo.content_id;
 
 --API를 통한 관광정보 등록
+INSERT INTO 
+  content (
+    type,
+    title
+  ) VALUE ('tour', ?);
+
+--INSERT 시 AI key 값 AI_ID에 저장
+SELECT
+  LAST_INSERT_ID ();
+
+INSERT INTO
+  tour (
+    id,
+    loc_category,
+    address,
+    info
+  ) VALUE (AI_ID, ?, ?, ?);
+
+INSERT INTO
+  photo (
+    photo,
+    content_id
+  ) VALUE (?, ?);
