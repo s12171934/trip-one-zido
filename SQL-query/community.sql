c72f069a88311d14c51f1b5ff325344af45b4838
MinSun <hsun0987@daum.net>
2024-02-27 10:33:29 +0900

    REFACTOR: community.sql

--- SQL-query/community.sql
+++ SQL-query/community.sql
@@ -3,7 +3,8 @@ SELECT
   c1.id,
   c2.title,
   o.member_id writer_id,
-  m.login_id writer c1.deadline,
+  m.login_id writer,
+  c1.deadline,
   c1.view_point,
   c1.total,
   c1.status,
@@ -14,7 +15,7 @@ SELECT
       owner
     WHERE
       content_id = id
-  ) total_count
+  ) participants_count
 FROM
   community c1,
   owner o,
