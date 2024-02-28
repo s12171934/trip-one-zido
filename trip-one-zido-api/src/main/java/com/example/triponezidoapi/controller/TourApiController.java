bc8e287b6305bf38c85584c5334cb0c65ec84069
MinSun <hsun0987@daum.net>
2024-02-28 13:20:25 +0900

    ADD, FEAT: TourApiController - 관광 컨트롤러 생성 - 조회, 등록 기능 추가

---
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/TourApiController.java
@@ -0,0 +1,21 @@
+package com.example.triponezidoapi.controller;
+
+import com.example.triponezidoapi.dto.Tour;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tour")
+public class TourApiController {
+    @GetMapping("/")
+    public Tour showTourList(){
+        return new Tour();
+    }
+    @GetMapping("/{id}")
+    public Tour showTourDetail(){
+        return new Tour();
+    }
+    @PostMapping("/")
+    public void addTour(@RequestBody Tour tour){
+
+    }
+}
