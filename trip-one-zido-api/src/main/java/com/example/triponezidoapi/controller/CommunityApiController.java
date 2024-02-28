3a68375378f2c375e59895819dda31d2254695c1
MinSun <hsun0987@daum.net>
2024-02-28 11:19:08 +0900

    ADD, FEAT: CommunityApiController - 전체조회, 상세조회, 등록, 수정, 삭제, 검색, 참여하기, 취소하기 기능 추가

---
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/CommunityApiController.java
@@ -0,0 +1,50 @@
+package com.example.triponezidoapi.controller;
+
+import com.example.triponezidoapi.dto.CommunityDetail;
+import com.example.triponezidoapi.dto.CommunityTable;
+import com.example.triponezidoapi.dto.ProfileMember;
+import com.example.triponezidoapi.dto.RequestSearch;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/community")
+public class CommunityApiController {
+
+    @GetMapping("/page={page}")
+    public List<CommunityTable> showCommunityList(@PathVariable int page){
+        return null;
+    }
+    @GetMapping("/{id}")
+    public CommunityDetail showCommunityDetail(@PathVariable long id){
+        return new CommunityDetail();
+    }
+    @GetMapping("/{id}/owner")
+    public ProfileMember showOwner(@PathVariable long id){
+        return new ProfileMember();
+    }
+    @PostMapping("/")
+    public void addCommunity(@RequestBody CommunityDetail communityDetail, long id){
+    }
+    @PutMapping("/{id}")
+    public void updateCommunity(@RequestBody CommunityDetail communityDetail, @PathVariable long id){
+
+    }
+    @DeleteMapping("/{id}")
+    public void deleteCommunity(@PathVariable long id){
+
+    }
+    @GetMapping("/search/page={page}")
+    public RequestSearch searchCommunity(@RequestBody CommunityTable communityTable, @PathVariable int page){
+        return new RequestSearch();
+    }
+    @PutMapping("entry/{id1}&{id2}")
+    public void entryCommunity(@PathVariable long id1, @PathVariable long id2){
+
+    }
+    @PutMapping("departure/{id1}&{id2}")
+    public void departureCommunity(@PathVariable long id1, @PathVariable long id2){
+
+    }
+}
