5780aa9d7ccc702cc54cfe42f7d4ff8e5182b200
MinSun <hsun0987@daum.net>
2024-02-28 13:56:25 +0900

    FEAT: SpotApiController - 장소 컨트롤러 생성 - 조회, 등록, 수정, 삭제 기능 추가

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/SpotApiController.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/SpotApiController.java
@@ -1,2 +1,47 @@
-package com.example.triponezidoapi.controller;public class SpotApiController {
+package com.example.triponezidoapi.controller;
+
+import com.example.triponezidoapi.dto.Photo;
+import com.example.triponezidoapi.dto.RequestOwner;
+import com.example.triponezidoapi.dto.Spot;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/spot")
+public class SpotApiController {
+    @GetMapping("/M{id}")
+    public Spot showSpotAll(){
+        return new Spot();
+    }
+    @GetMapping("/C{id}")
+    public Spot showSpotDetail(){
+        return new Spot();
+    }
+    @PostMapping("/")
+    public void addSpot(@RequestBody Spot spot){
+
+    }
+    @PutMapping("/")
+    public void updateSpot(@RequestBody Spot spot){
+
+    }
+    @PostMapping("/owner")
+    public void addOwner(@RequestBody RequestOwner requestOwner){
+
+    }
+    @DeleteMapping("/owner/{id}")
+    public void deleteOwner(@PathVariable long id){
+
+    }
+    @PostMapping("/photo")
+    public void addPhoto(@RequestBody Photo photo){
+
+    }
+    @DeleteMapping("/photo/{id}")
+    public void deletePhoto(@PathVariable long id){
+
+    }
+    @DeleteMapping("/C{id}")
+    public void deleteSpot(@PathVariable long id){
+
+    }
 }
