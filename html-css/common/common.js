1fbb26b1b8e0bc000bf2686e987a88a13d865d7e
ParkJhh <qkrwnsgur145@gmail.com>
2024-03-02 16:57:12 +0900

    REFACTOR: directory and code refactoring(html/css/js)

---
+++ html-css/common/common.js
@@ -0,0 +1,16 @@
+
+function zzim(){
+    alert("zzim버튼 확인?")
+}
+
+function like(){
+    alert("like버튼 확인?")
+}
+
+function unlike(){
+    alert("like버튼 확인?")
+}
+
+function deleteimg(){
+    alert("delete버튼 확인?")
+}
