e60525539ee606eebbdcf3f32d94146fc21283a1
ParkJhh <qkrwnsgur145@gmail.com>
2024-03-02 23:53:24 +0900

    REFACOTR : location directory and code refacotr

--- html-css/common/common.js
+++ html-css/common/common.js
@@ -14,3 +14,7 @@ function unlike(){
 function deleteimg(){
     alert("delete버튼 확인?")
 }
+
+function rateStar(stars) {
+    document.getElementById('ratingValue').innerText = '별점: ' + stars + '점';
+}
