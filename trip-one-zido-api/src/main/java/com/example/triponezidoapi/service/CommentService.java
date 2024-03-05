aa13dbfdb9775324a950f59f06020af80a2f7c07
MinSun <hsun0987@daum.net>
2024-03-05 17:40:51 +0900

    REFACTOR: Comment Service & Controller - 메소드명 수정 - import 정리

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/service/CommentService.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/service/CommentService.java
@@ -1,34 +1,28 @@
 package com.example.triponezidoapi.service;
 
-import com.example.triponezidoapi.dto.request.RequestComment;
-import com.example.triponezidoapi.dto.response.ResponseComment;
-import com.example.triponezidoapi.mappers.CommentMapper;
+import com.example.triponezidoapi.dto.request.*;
+import com.example.triponezidoapi.mappers.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CommentService {
     @Autowired
     CommentMapper commentMapper;
+    @Autowired
+    ContentMapper contentMapper;
 
-    //insert
     public void addComment(long sessionId, RequestComment requestComment){
         requestComment.setId(sessionId);
         commentMapper.addComment(requestComment);
     }
 
-    //select
-    public List<ResponseComment> getComment(long id){
-        List<ResponseComment> responseComments =  commentMapper.getComment(id);
-        return responseComments;
-    }
-
-    //update
     public void updateComment(long id, RequestComment requestComment){
         requestComment.setId(id);
         commentMapper.updateComment(requestComment);
     }
 
+    public void deleteComment(long id){
+        contentMapper.deleteContent(id);
+    }
 }
