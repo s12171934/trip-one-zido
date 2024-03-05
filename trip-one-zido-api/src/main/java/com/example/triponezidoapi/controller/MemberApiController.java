3b0e349c5f30b460a7441f93e253dc95037adb83
MinSun <hsun0987@daum.net>
2024-03-05 10:58:10 +0900

    FEAT: MemberService, MemberApiController - 매퍼, 서비스, 컨트롤러 연결 중

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/MemberApiController.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/MemberApiController.java
@@ -3,6 +3,7 @@ package com.example.triponezidoapi.controller;
 import com.example.triponezidoapi.dto.*;
 import com.example.triponezidoapi.dto.request.*;
 import com.example.triponezidoapi.dto.response.*;
+import com.example.triponezidoapi.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,7 +17,7 @@ public class MemberApiController {
     @GetMapping("/signup")
     @Operation(summary = "회원가입시 보안질문 조회")
     public List<ResponseQuestions> questionsInfo(){
-        return null;
+        return MemberService.getSecurityQuestions();
     }
     @PostMapping("/signup")
     @Operation(summary = "회원가입")
@@ -25,7 +26,7 @@ public class MemberApiController {
             @Parameter(description = "회원 등록 정보")
             Member member
     ){
-
+        MemberService.signUp(member);
     }
     @PostMapping("/signup/{loginId}")
     @Operation(summary = "아이디 중복확인")
@@ -34,7 +35,7 @@ public class MemberApiController {
             @Parameter(description = "작성한 아이디")
             String loginId
     ){
-
+        MemberService.isUsingMemberId(loginId);
     }
     @PostMapping("/login")
     @Operation(summary = "로그인")
@@ -43,7 +44,7 @@ public class MemberApiController {
             @Parameter(description = "로그인 정보")
             Login login
     ){
-
+        MemberService.login(login);
     }
     @PostMapping("/check/id")
     @Operation(summary = "아이디 찾기")
@@ -52,7 +53,7 @@ public class MemberApiController {
             @Parameter(description = "이름 / 이메일")
             RequestFind requestFind
     ){
-        return null;
+        return MemberService.getLoginId(requestFind);
     }
     @PostMapping("/check/pw")
     @Operation(summary = "비밀번호 찾기")
@@ -61,7 +62,7 @@ public class MemberApiController {
             @Parameter(description = "이름 / 이메일 / 아이디")
             RequestFind requestFind
     ){
-        return 0;
+        return MemberService.getId(requestFind);
     }
     @GetMapping("/check/{id}")
     @Operation(summary = "비밀번호 찾기 - 선택한 보안 질문 조회")
@@ -70,7 +71,7 @@ public class MemberApiController {
             @Parameter(description = "회원 번호")
             long id
     ){
-        return null;
+        return MemberService.getQuestion(id);
     }
     @PostMapping("/check/{id}")
     @Operation(summary = "비밀번호 찾기 - 보안 답변 전송")
@@ -83,7 +84,7 @@ public class MemberApiController {
             @Parameter(description = "보안 답변")
             String answer
     ){
-
+        MemberService.checkAnswer(id, answer);
     }
     @PutMapping("/passwd/{id}")
     @Operation(summary = "비밀번호 찾기 - 비밀번호 재설정")
@@ -96,7 +97,7 @@ public class MemberApiController {
             @Parameter(description = "새로운 비밀번호")
             RequestNewPassword requestNewPassword
     ){
-
+        MemberService.updatePw(id, requestNewPassword);
     }
     @GetMapping("/")
     @Operation(summary = "회원 정보 조회")
@@ -105,7 +106,7 @@ public class MemberApiController {
             @Parameter(description = "로그인 회원 번호")
             long SessionId
     ){
-        return null;
+        return MemberService.getMember(SessionId);
     }
     @PutMapping("/")
     @Operation(summary = "회원 정보 수정")
@@ -118,7 +119,7 @@ public class MemberApiController {
             @Parameter(description = "수정할 회원 정보")
             Member member
     ){
-
+        MemberService.updateMember(sessionId, member);
     }
     @PutMapping("/profile")
     @Operation(summary = "프로필 사진 변경")
@@ -131,7 +132,7 @@ public class MemberApiController {
             @Parameter(description = "프로필 사진")
             byte[] profile
     ){
-
+        MemberService.updateProfile(sessionId, profile);
     }
     @PutMapping("/password")
     @Operation(summary = "비밀번호 변경")
@@ -144,7 +145,7 @@ public class MemberApiController {
             @Parameter(description = "변경할 비밀번호")
             RequestNewPassword requestNewPassword
     ){
-
+        MemberService.updatePw(sessionId, requestNewPassword);
     }
     @DeleteMapping("/")
     @Operation(summary = "회원 탈퇴")
@@ -153,7 +154,7 @@ public class MemberApiController {
             @Parameter(description = "로그인 회원 정보")
             long id
     ){
-
+        MemberService.deleteMember(id);
     }
 /*    @PostMapping("/pairing")
     public void pairingSignup(@RequestBody ){
--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/service/MemberService.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/service/MemberService.java
@@ -1,4 +1,88 @@
 package com.example.triponezidoapi.service;
 
+import com.example.triponezidoapi.dto.*;
+import com.example.triponezidoapi.dto.request.*;
+import com.example.triponezidoapi.dto.response.*;
+import com.example.triponezidoapi.mappers.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@Service
 public class MemberService {
+
+    @Autowired
+    static MemberMapper memberMapper;
+
+    public static List<ResponseQuestions> getSecurityQuestions(){
+        return memberMapper.getSecurityQuestionList();
+    }
+
+    public static void signUp(Member member){
+        // 회원 가입 제약 조건 추가
+        memberMapper.signUp(member);
+    }
+
+    public static void isUsingMemberId(String memberId){
+        memberMapper.getLoginFormByLoginId(memberId);
+    }
+
+    public static void login(Login login){
+        // 로그인 정보가 맞는지 확인
+        // 세션에 회원번호, 아아디, 프로필사진 저장
+        memberMapper.getLogin(login);
+    }
+
+    public static String getLoginId(RequestFind requestFind){
+       return memberMapper.getLoginIdByFind(requestFind);
+    }
+
+    public static long getId(RequestFind requestFind){
+        return memberMapper.getId(requestFind);
+    }
+
+    public static String getQuestion(long id){
+        return memberMapper.getSecurityQuestion(id);
+    }
+
+    public static void checkAnswer(long id, String myAnswer){
+       String answer = memberMapper.getSecurityAnswer(id);
+       // 보안질문이 맞는지 확인
+       if(myAnswer.equals(answer)){
+
+       }else
+          ;
+    }
+
+    public static void updatePw(long id, RequestNewPassword requestNewPassword){
+        // requestNewPassword 에서 비밀번호 제약조건 확인 후 requestPassword에 넣어서 전송
+        RequestPassword requestPassword = new RequestPassword();
+        requestPassword.setId(id);
+        requestPassword.setPassword(requestNewPassword.getNowPassword());
+        memberMapper.updatePassword(requestPassword);
+    }
+
+    public static Member getMember(long id){
+        return memberMapper.getMemberById(id);
+    }
+
+    public static void updateMember(long id, Member member){
+        member.setId(id);
+        memberMapper.updateMember(member);
+    }
+
+    public static void updateProfile(long id, byte[] profile){
+        // 받은 아이디와 프로필을 requestPhoto에 저장 후 매퍼로 전송
+        RequestPhoto requestPhoto = new RequestPhoto();
+        requestPhoto.setContentId(id);
+        requestPhoto.setPhoto(profile);
+        memberMapper.updateProfile(requestPhoto);
+    }
+
+    public static void deleteMember(long id){
+        memberMapper.deleteMember(id);
+    }
+
 }
