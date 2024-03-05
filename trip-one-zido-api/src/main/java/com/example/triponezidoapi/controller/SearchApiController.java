de1c8f252d02fb01a17869a9359a4941747594fa
MinSun <hsun0987@daum.net>
2024-03-05 15:52:27 +0900

    FEAT: SearchService, REFACTOR: SearchApiController - controller의 searchKeyword 매개변수에 세션아이디 추가 - search service 틀 구현

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/SearchApiController.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/SearchApiController.java
@@ -2,29 +2,37 @@ package com.example.triponezidoapi.controller;
 
 import com.example.triponezidoapi.dto.request.*;
 import com.example.triponezidoapi.dto.response.*;
+import com.example.triponezidoapi.service.SearchService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/search")
 @Tag(name = "Search", description = "Search API")
 public class SearchApiController {
+    @Autowired
+    SearchService searchService;
+
     @GetMapping("/{keyword}")
     @Operation(summary = "기본 검색")
     public ResponseSearch searchKeyword(
+            @SessionAttribute(name="id")
+            @Parameter(description = "로그인 회원 번호")
+            long sessionId,
             @PathVariable
             @Parameter(description = "검색어")
             String keyword){
-        return null;
+        return searchService.searchByKeyword(sessionId, keyword);
     }
     @GetMapping("/detail")
     @Operation(summary = "상세 검색")
-    public ResponseSearch searchPlan(
+    public ResponseSearch searchDetail(
             @RequestBody
             @Parameter(description = "상세 검색 조건")
             RequestDetailSearch detailSearch){
-        return null;
+        return searchService.searchByDetail(detailSearch);
     }
 }
