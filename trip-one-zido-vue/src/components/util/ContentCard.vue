2cd53f6f1145a69b27f47f8c5ac2b2e83be3e534
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-03-05 16:12:18 +0900

    FEATURE: contentCard LIst components -조건식을 이용하여 장소 일정 관광지임을 구분하여 별도의 컴포넌트가 필요하지않다고 판단, 통합으로 진행함

---
+++ trip-one-zido-vue/src/components/util/ContentCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div @click="pushDetail" :id="content.id" class="container">
+    <div class="image">
+      <img :src="content.imgSrc" class="img" />
+    </div>
+    <div v-if="content.type != null" class="hover-effect flex-column">
+      <span
+        ><img id="bookmark" src="/images/zzim.png" />&nbsp;{{
+          content.bookmarkCount
+        }}</span
+      >
+      <span v-if="content.type != 'tour'"
+        ><img id="star" src="/images/star.png" />&nbsp;{{ content.grade }}</span
+      >
+      <span v-if="content.type != 'tour'"
+        ><img id="like" src="/images/like.png" />&nbsp;{{ content.like }}</span
+      >
+    </div>
+    <p>
+      {{ content.title }}&nbsp;<img
+        v-if="content.type != null"
+        @click="$emit('toggle', content)"
+        id="bookmark"
+        :src="content.myBookmark ? '/images/zzim.png' : '/images/unzzim.png'"
+      />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: Object,
+  },
+  methods: {
+    pushDetail() {
+      if(this.content.type != null){
+        this.$router.push({ path: `/${this.content.type}/${this.content.id}` });
+      } else{
+        this.$emit('plus')
+      }
+      
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  height: 360px;
+  width: 300px;
+  padding: 20px;
+}
+.image {
+  border-radius: 20%;
+  overflow: hidden;
+  margin-bottom: 10px;
+}
+.img {
+  height: 260px;
+  width: 260px;
+}
+.hover-effect {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  border-radius: 20%;
+  height: 260px;
+  width: 260px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  opacity: 0;
+}
+.hover-effect:hover {
+  opacity: 70%;
+}
+p {
+  line-height: 28px;
+  font-family: "Jalnan";
+  align-items: center;
+  text-align: center;
+  margin: 0;
+}
+span {
+  display: flex;
+  align-items: center;
+  line-height: 30px;
+  font-family: "Jalnan";
+  color: white;
+}
+#bookmark,
+#star,
+#like {
+  width: 25px;
+  height: 25px;
+}
+</style>
