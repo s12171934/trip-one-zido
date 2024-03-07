60e622b6c39e3826a2a0c2b3ac88dc53426afa12
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-03-07 12:10:18 +0900

    REFACTOR: tour components

--- trip-one-zido-vue/src/components/tour/TourPage.vue
+++ trip-one-zido-vue/src/components/tour/TourPage.vue
@@ -1,48 +1,56 @@
 <template>
   <section id="main" class="wrapper">
-    <div id="listTitle">
-      <h1>이런 곳 어때요?</h1>
-    </div>
-    <div class="select-wrapper" style="display: flex; justify-content: center">
-      <div class="d-flex flex-row mb-3">
-        <div class="p-2">
-          <select
-            class="button alt rounded-3"
-            name="category"
-            id="paldo"
-            style="width: 200px"
-          >
-            <option value="" selected>도 선택 ▼</option>
-            <option value="1">서울특별시</option>
-            <option value="2">경기도</option>
-            <option value="3">강원도</option>
-            <option value="4">충청북도</option>
-            <option value="5">충청남도</option>
-            <option value="6">전라북도</option>
-            <option value="7">전라남도</option>
-            <option value="8">경상북도</option>
-            <option value="9">경상남도</option>
-            <option value="10">제주도</option>
-          </select>
-        </div>
-      </div>
+    <div class="d-flex p-2">
+      <span>이런 곳 어때요?</span>
+      <select class="button alt" id="locSelect" name="location" v-model="loc">
+        <option v-for="location in selectLocations" :value="location">
+          {{ location }}
+        </option>
+      </select>
     </div>
 
-    <div id="mainTitle">
-      <h1>・부산</h1>
-    </div>
     <ContentList />
   </section>
 </template>
 
 <script>
 import ContentList from "../util/ContentList.vue";
+import data from "/src/assets/data.js";
 
 export default {
   components: {
     ContentList,
   },
+  data() {
+    return {
+      selectLocations: data.selectLocations,
+      loc: "서울특별시",
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+span {
+  line-height: 60px;
+  font-size: 30px;
+  margin-right: 30px;
+}
+
+#locSelect {
+  box-shadow: none !important;
+  border: none;
+  font-family: "Jalnan";
+  color: #ff928e !important;
+  font-size: 30px;
+  text-align: left;
+  padding: 0;
+  width: 300px;
+  height: 60px;
+  line-height: 60px;
+}
+
+#locSelect:hover {
+  background-color: #fff;
+}
+</style>
