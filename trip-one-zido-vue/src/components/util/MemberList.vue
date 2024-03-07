6a8647153aa3cba18545a1bfc7b9ae9e4d2dd2e4
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-03-07 11:23:52 +0900

    REFACTOR: search components

---
+++ trip-one-zido-vue/src/components/util/MemberList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="d-flex flex-row mb-6" id="searchUser">
+    <div v-for="profile in userProfiles" class="p-2">
+      <img :src="profile.imgSrc"/>
+      <p>{{ profile.loginId }}</p>
+    </div>
+    <div class="p-2">
+      <img src="/images/plusbutton.png"/>
+      <p>더보기</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from "/src/assets/data.js";
+
+export default {
+  data() {
+    return {
+      userProfiles: data.userProfiles,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#searchUser {
+  margin: 0;
+}
+
+img {
+  width: 180px;
+  height: 180px;
+  border-radius: 100%;
+  margin-bottom: 30px;
+}
+p {
+  text-align: center;
+}
+</style>
