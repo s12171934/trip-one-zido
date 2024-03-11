4413a3a3b5b29e4e39ad9214eab39652a95fb4ee
MinSun <hsun0987@daum.net>
2024-03-11 16:00:22 +0900

    FEAT: DetailSearch 기간 검색 - 사계절에 따른 기간 검색 기능 추가

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/dto/request/RequestDetailSearch.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/dto/request/RequestDetailSearch.java
@@ -2,15 +2,14 @@ package com.example.triponezidoapi.dto.request;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class RequestDetailSearch {
     Long myMemberId;
     String keyword;
     String locCategory;
     String category;
-    LocalDateTime startDate;
-    LocalDateTime endDate;
+    String season;
+    int startMonth;
+    int endMonth;
     long page;
 }
