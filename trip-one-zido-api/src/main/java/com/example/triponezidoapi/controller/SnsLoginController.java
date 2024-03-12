eaa62cf88e4d1ba2e0972edc7ed9af328773e4c7
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-03-12 13:50:01 +0900

    FEATURE: sns Login

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/SnsLoginController.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/controller/SnsLoginController.java
@@ -1,22 +1,14 @@
 package com.example.triponezidoapi.controller;
 
 import com.example.triponezidoapi.service.SnsLoginService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/social")
@@ -24,12 +16,14 @@ public class SnsLoginController {
     @Autowired
     SnsLoginService snsLoginService;
     @GetMapping("/naver_callback")
-    void naverLogin(@Parameter String code, @Parameter String state) throws JsonProcessingException{
-        snsLoginService.naverLogin(code,state);
+    void naverLogin(@Parameter String code, @Parameter String state, HttpServletResponse response) throws IOException {
+        String id = snsLoginService.naverLogin(code,state);
+        response.sendRedirect("http://localhost:3000/social/naver/" + id);
     }
 
     @GetMapping("/kakao_login")
-    void kakoLogin(@Parameter String code) throws JsonProcessingException {
-        snsLoginService.kakaoLogin(code);
+    void kakoLogin(@Parameter String code, HttpServletResponse response) throws IOException {
+        String id = snsLoginService.kakaoLogin(code);
+        response.sendRedirect("http://localhost:3000/social/kakao/" + id);
     }
 }
