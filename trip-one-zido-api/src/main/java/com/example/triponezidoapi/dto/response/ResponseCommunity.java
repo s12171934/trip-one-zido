7f6f3f2d58bc6c338c7054d812bdad5e6bb21b10
K-Sungsil <lovemykth@naver.com>
2024-03-14 15:37:11 +0900

    Test : CommunityApiControllerTest

--- trip-one-zido-api/src/main/java/com/example/triponezidoapi/dto/response/ResponseCommunity.java
+++ trip-one-zido-api/src/main/java/com/example/triponezidoapi/dto/response/ResponseCommunity.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 @Data
 public class ResponseCommunity {
-    long id;
+    Long id;
     String title;
     String writer;
     LocalDateTime deadline;
@@ -13,4 +13,15 @@ public class ResponseCommunity {
     int total;
     int withCount;
     String status;
+
+    public ResponseCommunity(Long id, String title, String writer, LocalDateTime deadline, int viewPoint, int total, int withCount, String status) {
+        this.id = id;
+        this.title = title;
+        this.writer = writer;
+        this.deadline = deadline;
+        this.viewPoint = viewPoint;
+        this.total = total;
+        this.withCount = withCount;
+        this.status = status;
+    }
 }
