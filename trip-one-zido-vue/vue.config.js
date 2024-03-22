3ceb309e3896e56fb290c84a6a16a908e64a5960
MinSun <hsun0987@daum.net>
2024-03-22 15:43:07 +0900

    FIX: sns login callback url (+9 squashed commit)
    Squashed commit:
    
    [df7564e] REFACOR: to fit aws
    
    [f92c506] DOCS: docker & REMOVE: prev folder
    
    [4b2f3e0] DOCS: docker-compose
    
    [544251f] ﻿FIX: to fit docker
    
    [9deb765] FEATURE: docker
    
    [a807148] FEAT: locMap
    
    - 지역 방문 횟수에 따른 지도 표시 기능
    - getLocMap 컨트롤러 & 서비스 & axios 추가
    - MemberPage, KakaoMapForMemberPage 뷰 수정
    
    [b92222d] TEST : test code
    
    [65101c7] FEAT : locCountList add
    - 회원 페이지에 표시되는 지역 카테고리별 횟수 세는 쿼리, 기능 추가 수정
    
    [b0210cd] REFACTOR : drop pin table

--- trip-one-zido-vue/vue.config.js
+++ trip-one-zido-vue/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {	
-        target: "http://localhost:8080", // 프록시를 설정할 도메인
+        target: "http://13.60.3.28:8080", // 프록시를 설정할 도메인
         changeOrigin: true,
       }, 
     }
